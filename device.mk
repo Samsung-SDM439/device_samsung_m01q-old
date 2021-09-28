f85534dc31d861c99e4b5f40f8efe90c4fd36ead
Sushrut1101 <guptasushrut@gmail.com>
2021-09-28 16:13:16 +0530

    m01q: Create device.mk

--- device.mk
+++ device.mk
@@ -13,13 +13,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
-
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Inherit from m01q device
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
