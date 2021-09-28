0c504239337549a52adf0d7c479c06ce171eb6ad
Sushrut Gupta <guptasushrut@gmail.com>
2021-09-28 16:17:27 +0530

    m01q: Define Architecture

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,3 +15,10 @@
 #
 
 DEVICE_PATH := device/samsung/m01q
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := generic
