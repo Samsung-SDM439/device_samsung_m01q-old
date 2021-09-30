4855e7c0567f205d300c0e890a98370c80051253
TTTT5 <iamtttt55@gmail.com>
2021-09-30 19:09:37 +0530

    m01q: Import framework_manifest from the Stock ROM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -100,6 +100,7 @@ endif
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
 
 # Security Patch
 VENDOR_SECURITY_PATCH := 2021-07-01
