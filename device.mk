e281c1bb01c4c12b576f7d38e1a7563322aa7463
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2021-10-21 18:50:36 +0530

    m01q: Enable FreeForm Windows
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>
    Signed-off-by: TTTT55 <iamtttt55@gmail.com>
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -151,6 +151,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
